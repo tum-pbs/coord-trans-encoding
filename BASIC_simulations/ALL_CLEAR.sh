9cfffc11d30174a6ee930537ef7933f021823a1d
ge35mav <ge35mav@tum.de>
2022-07-29 15:26:44 +0200

    liwei: clean the simulation folder

---
+++ BASIC_simulations/ALL_CLEAR.sh
@@ -0,0 +1,8 @@
+rm cfl3d*
+rm *.bin
+rm *.dat
+rm input_1.dat
+rm temp*.inp
+rm -rf run_files train train_avg train_mesh history_files
+rm plot3d_To_p3d 
+rm avg_To_p3d
---
+++ BASIC_simulations/prep.sh
@@ -0,0 +1,5 @@
+mkdir run_files train train_avg train_mesh history_files
+
+gfortran plot3d_To_p3d.f90 -o plot3d_To_p3d
+
+gfortran avg_To_p3d.f90 -o avg_To_p3d
